3d63ed040ebbf7478732bec36e839cff5a351c62
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2020-11-30 00:01:38 -0600

    add updated files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,6 +9,64 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+//connect
+mongoose.connect("mongodb://localhost:27017/flyrestDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//schema
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    address: String,
+    city: String,
+    state: String,
+    country: String,
+    zipCode: String,
+    email: String,
+    userName: String,
+    password: String
+});
+
+const flightSchema = new mongoose.Schema({
+    from: String,
+    to: String,
+    departure: Date,
+    return: Date,
+    travelers: Number
+});
+
+const hotelSchema = new mongoose.Schema({
+    city: String,
+    to: String,
+    departure: Date,
+    return: Date,
+    rooms: Number,
+    guests: Number
+});
+
+//model
+const User = mongoose.model("User", userSchema);
+const Flight = mongoose.model("Flight", flightSchema);
+const Hotel = mongoose.model("Hotel", hotelSchema);
+
+//create
+
+// const newUser = new Fly({
+//     firstName: "Sarath",
+//     lastName: "Reddy",
+//     userName: "ssmb1045",
+//     email: "scrn@gmail.com",
+//     password: "donno"
+// });
+
+// newUser.save(function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("User added to database");
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render("index");
@@ -21,12 +80,32 @@ app.get("/hotel", function(req, res){
     res.render("hotel");
 });
 
-app.get("/blog", function(req, res){
-    res.render("blog");
+app.get("/flightHotel", function(req, res){
+    res.render("flightHotel");
 });
 
-app.get("/contact", function(req, res){
-    res.render("contact");
+app.get("/deals", function(req, res){
+    res.render("deals");
+});
+
+app.get("/flightStatus", function(req, res){
+    res.render("flightStatus");
+});
+
+app.get("/mileage", function(req, res){
+    res.render("mileage");
+});
+
+app.get("/feedback", function(req, res){
+    res.render("feedback");
+});
+
+app.get("/signup", function(req, res){
+    res.render("signup");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
 });
 
 app.listen(process.env.PORT || 3000, function(){
